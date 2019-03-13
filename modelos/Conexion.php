bcd3458bcd10b9b5630e18266eecafc71417d97f
Fernando Gaitán <fernandoggaitan@gmail.com>
2019-03-13 10:54:09 -0300

    Update Conexion.php

--- modelos/Conexion.php
+++ modelos/Conexion.php
@@ -14,7 +14,7 @@ class Conexion extends PDO {
     public function __construct() {
         //Sobreescribo el método constructor de la clase PDO.
         try {
-            parent::__construct($this->tipo_de_base . ':host=' . $this->host . ';dbname=' . $this->nombre_de_base, $this->usuario, $this->contrasena);
+            parent::__construct("{$this->tipo_de_base}:dbname={$this->nombre_de_base};host={$this->host};charset=utf8", $this->usuario, $this->contrasena);
         } catch (PDOException $e) {
             echo 'Ha surgido un error y no se puede conectar a la base de datos. Detalle: ' . $e->getMessage();
             exit;
