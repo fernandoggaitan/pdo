29e87a634a6045be434ca6328f8b0394b06fde5c
Fernando Gaitán <fernandoggaitan@gmail.com>
2023-10-10 22:39:03 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     require_once 'modelos/Personaje.php';
     
     $personajes = Personaje::recuperarTodos();
-    
+
+    //Modifico algo para probar.
     require_once 'vistas/index.php';
     
 ?>
